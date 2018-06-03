3a4928dd65583d13b522bc19c369a06f355eba1c
Maarten de Vries <maarten@de-vri.es>
2018-06-04 01:20:09 +0200

    Add more geometry utilities.

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,8 +1,11 @@
 use nalgebra::{
+	Affine3,
+	Matrix4,
 	Real,
 	Rotation3,
+	Translation3,
 	Unit,
-	Vector3
+	Vector3,
 };
 
 use alga::linear::NormedSpace;
@@ -24,3 +27,37 @@ pub fn rotate<T: Real>(axis: Unit<Vector3<T>>, angle: T) -> Rotation3<T> {
 pub fn rotate_x<T: Real>(angle: T) -> Rotation3<T> { rotate(Vector3::x_axis(), angle) }
 pub fn rotate_y<T: Real>(angle: T) -> Rotation3<T> { rotate(Vector3::y_axis(), angle) }
 pub fn rotate_z<T: Real>(angle: T) -> Rotation3<T> { rotate(Vector3::z_axis(), angle) }
+
+pub fn translate<T: Real>(amount: Vector3<T>) -> Translation3<T> {
+	Translation3::from_vector(amount)
+}
+
+pub fn translate_x<T: Real>(distance: T) -> Translation3<T> { translate(Vector3::x_axis().unwrap() * distance) }
+pub fn translate_y<T: Real>(distance: T) -> Translation3<T> { translate(Vector3::y_axis().unwrap() * distance) }
+pub fn translate_z<T: Real>(distance: T) -> Translation3<T> { translate(Vector3::z_axis().unwrap() * distance) }
+
+pub fn scale<T: Real>(factors: Vector3<T>) -> Affine3<T> {
+	let mut result = Matrix4::<T>::identity();
+	result[(0, 0)] = factors[0];
+	result[(1, 1)] = factors[1];
+	result[(2, 2)] = factors[2];
+	Affine3::from_matrix_unchecked(result)
+}
+
+pub fn scale_x<T: Real>(f: T) -> Affine3<T> {
+	let mut result = Matrix4::<T>::identity();
+	result[(0, 0)] = f;
+	Affine3::from_matrix_unchecked(result)
+}
+
+pub fn scale_y<T: Real>(f: T) -> Affine3<T> {
+	let mut result = Matrix4::<T>::identity();
+	result[(0, 0)] = f;
+	Affine3::from_matrix_unchecked(result)
+}
+
+pub fn scale_z<T: Real>(f: T) -> Affine3<T> {
+	let mut result = Matrix4::<T>::identity();
+	result[(0, 0)] = f;
+	Affine3::from_matrix_unchecked(result)
+}
